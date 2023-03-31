b060352417a00ee823b70402e8d0ae8fefa60c2d
YannisHaismann <YannisHaismann@outlook.fr>
2023-03-31 08:14:50 +0200

    feat: mui theme & default primary color

--- components/HomepageFilterButtons.tsx
+++ components/HomepageFilterButtons.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 import React from 'react';
 
 const CATEGORIES: { displayName: string; id: string }[] = [
@@ -28,20 +30,25 @@ export default function HomepageFilterButtons({
   clickable,
 }: any) {
   return (
-    <div className="mt-8 mb-16 bg-white rounded-lg border-gray-400 border-[1px] p-2 w-[700px]">
-      {CATEGORIES.map((el) => (
-        <button
-          key={el.id}
-          onClick={() => {
-            if (clickable) {
-              changeContentType(el.id);
-            }
-          }}
-          className="mr-4 p-2 hover:bg-gray-400 hover:rounded-full"
-        >
-          {el.displayName}
-        </button>
-      ))}
+    <div className="mt-8 mb-16 flex justify-between bg-white rounded-lg border-gray-400 border-[1px] p-2 w-[700px]">
+      <div>
+        {CATEGORIES.map((el) => (
+          <button
+            key={el.id}
+            onClick={() => {
+              if (clickable) {
+                changeContentType(el.id);
+              }
+            }}
+            className="mr-4 p-2 hover:bg-gray-400 hover:rounded-full"
+          >
+            {el.displayName}
+          </button>
+        ))}
+      </div>
+      <Button variant="outlined" startIcon={<AddIcon />}>
+        Ajouter
+      </Button>
     </div>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,19 @@ import { Provider } from 'react-redux';
 import { store } from './../Redux/Store';
 import { AuthProvider } from '@/context/AuthContext';
 import { CookiesProvider } from 'react-cookie';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import { theme } from './../utils/muiTheme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <CookiesProvider>
-        <AuthProvider>
-          <Component {...pageProps} />
-        </AuthProvider>
-      </CookiesProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <CookiesProvider>
+          <AuthProvider>
+            <Component {...pageProps} />
+          </AuthProvider>
+        </CookiesProvider>
+      </Provider>
+    </ThemeProvider>
   );
 }
