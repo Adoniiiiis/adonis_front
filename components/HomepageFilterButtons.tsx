ed4fd93ced40711e641ec195b6f025f5d6a270e3
YannisHaismann <YannisHaismann@outlook.fr>
2023-03-31 07:44:48 +0200

    fix: HomePageFilterButton.tsx upgrade

--- components/HomepageFilterButtons.tsx
+++ components/HomepageFilterButtons.tsx
@@ -1,65 +1,47 @@
 import React from 'react';
 
+const CATEGORIES: { displayName: string; id: string }[] = [
+  {
+    displayName: 'Populaires',
+    id: 'popular',
+  },
+  {
+    displayName: 'Nouveaux',
+    id: 'newContent',
+  },
+  {
+    displayName: 'Livres',
+    id: 'books',
+  },
+  {
+    displayName: 'Vidéos',
+    id: 'videos',
+  },
+  {
+    displayName: 'Citations',
+    id: 'quotes',
+  },
+];
+
 export default function HomepageFilterButtons({
   changeContentType,
   clickable,
 }: any) {
   return (
     <div className="mt-8 mb-16 bg-white rounded-lg border-gray-400 border-[1px] p-2 w-[700px]">
-      <button
-        onClick={() => {
-          if (clickable) {
-            changeContentType('popular');
-          }
-        }}
-        className="mr-4 p-2 hover:bg-gray-400 hover:rounded-full"
-      >
-        Populaires
-      </button>
-
-      <button
-        onClick={() => {
-          if (clickable) {
-            changeContentType('newContent');
-          }
-        }}
-        className="mr-4 p-2 hover:bg-gray-400 hover:rounded-full"
-      >
-        Nouveaux
-      </button>
-
-      <button
-        onClick={() => {
-          if (clickable) {
-            changeContentType('books');
-          }
-        }}
-        className="mr-4 p-2 hover:bg-gray-400 hover:rounded-full"
-      >
-        Livres
-      </button>
-
-      <button
-        onClick={() => {
-          if (clickable) {
-            changeContentType('videos');
-          }
-        }}
-        className="mr-4 p-2 hover:bg-gray-400 hover:rounded-full"
-      >
-        Vidéos
-      </button>
-
-      <button
-        onClick={() => {
-          if (clickable) {
-            changeContentType('quotes');
-          }
-        }}
-        className="mr-4 p-2 hover:bg-gray-400 hover:rounded-full"
-      >
-        Citations
-      </button>
+      {CATEGORIES.map((el) => (
+        <button
+          key={el.id}
+          onClick={() => {
+            if (clickable) {
+              changeContentType(el.id);
+            }
+          }}
+          className="mr-4 p-2 hover:bg-gray-400 hover:rounded-full"
+        >
+          {el.displayName}
+        </button>
+      ))}
     </div>
   );
 }
